a302de57e6160d25a949548bc82899b1de2e44ca
Sameer <syedsameershah2008@gmail.com>
2026-03-12 19:34:29 +0500

    box

--- src/components/custom/sections/ReadyToMake.jsx
+++ src/components/custom/sections/ReadyToMake.jsx
@@ -3,29 +3,36 @@ import React from 'react'
 
 const ReadyToMake = () => {
     return (
-        <div className=" bg-[linear-gradient(102.16deg,#B384EF_0%,#7A28E1_100%,#FFFFFF_100%)] rounded-[15.6px] p-8 text-white">
-            import Image from "next/image"
-        <img src="/stats.png"/>
-            {/* <Image
+        <div className="max-w-450 m-auto flex justify-between bg-[linear-gradient(102.16deg,#B384EF_0%,#7A28E1_100%,#FFFFFF_100%)] rounded-3xl text-white">
+           
+            <Image
                 src="/stats.png"
                 alt="Business growth illustration"
                 width={800}
                 height={600}
-                className="w-full h-auto object-cover rounded-xl"
-                sizes="(max-width: 640px) 100vw,
-         (max-width: 1024px) 50vw,
-         800px"
+                className="w-auto max-w-full h-auto rounded-xl"
+                sizes="(max-width: 768px) 100vw, 800px"
                 priority
-            /> */}
-            <h3 className="text-2xl font-semibold">
-                Ready To Take Your Business To The Next Level?
-            </h3>
+            />
+            <div className="content flex flex-col items-center justify-center">
+                <h3 className="text-2xl font-semibold">
+                    Ready To Take Your Business To The Next Level?
+                </h3>
 
-            <p className="mt-3 text-sm text-white/90">
-                Let’s turn your vision into a powerful digital experience. Partner with us to grow smarter,
-                faster, and stronger in today’s digital world.
-            </p>
-            <Image />
+                <p className="mt-3 text-sm text-white/90">
+                    Let’s turn your vision into a powerful digital experience. Partner with us to grow smarter,
+                    faster, and stronger in today’s digital world.
+                </p>
+            </div>
+            <Image
+                src="/stats2.png"
+                alt="Business growth illustration"
+                width={800}
+                height={600}
+                className="w-auto max-w-full h-auto rounded-xl"
+                sizes="(max-width: 768px) 100vw, 800px"
+                priority
+            />
         </div>
     )
 }
